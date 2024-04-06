7d4f529102fc770732f48dc985b31c99f46b694e
Nick Neos <nicholas.neos@gmail.com>
2024-04-06 20:16:49 +1100

    added docstrings

--- ha_notify/__init__.py
+++ ha_notify/__init__.py
@@ -1,1 +1,3 @@
+""" This package contains the class `HomeAssistantNotify`"""
+
 from .ha_notify import HomeAssistantNotify
--- ha_notify/ha_notify.py
+++ ha_notify/ha_notify.py
@@ -6,18 +6,41 @@ logger = logging.getLogger(__name__)
 
 
 class HomeAssistantNotify:
+    """
+    A class to represent a `HomeAssistantNotify` object.
+
+    Attributes:
+        url (str): The url and endpoint to send the request to.
+        headers (dict): headers to include in the http post request.
+    """
 
     def __init__(
         self,
         ha_url="http://127.0.0.1:8123",
         token=None,
-        endpoint=None,
+        endpoint="/api/services/notify/notify",
     ):
+        """Initialize `HomeAssistantNotify` class.
+
+        Args:
+            ha_url (str): The URL of your Home Assistant server. Defaults to `http://127.0.0.1:8123`.
+            token (str): Your home assistant api token.
+            endpoint (str): The endpoint to send the .
+        """
         self.url = os.path.join(ha_url, endpoint)
         self.headers = {"Authorization": f"Bearer {token}"}
 
-    def send_msg(self, **kvargs):
-        response = post(self.url, headers=self.headers, json=kvargs)
+    def send_msg(self, message, **kwargs):
+        """Sends a message via home assistants notify service.
+
+        Args:
+            message (str): The message to send in the notification
+            **kwargs: Additional key value pairs to include in the payload.
+                        Refer to https://www.home-assistant.io/integrations/notify#service
+                        for additional optional keys.
+        """
+        payload = {"message": message, **kwargs}
+        response = post(self.url, headers=self.headers, json=payload)
         if response.status_code == 200:
             logger.info("Sent notification via HA")
         else:
